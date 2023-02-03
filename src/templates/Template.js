ebe5ab49ed1a62c0cd464c519065a0cb3a8a8e1f
Manuel Agudelo <juan.manuelillo02@gmail.com>
2023-02-02 20:18:58 -0500

    FistdeBug

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,4 +1,7 @@
 import getData from '../utils/getData.js';
+import github from '../assets/images/github.png';
+import twitter from '../assets/images/twitter.png';
+import instagram from '../assets/images/instagram.png';
 
 const Template = async () => {
   const data = await getData();
@@ -23,14 +26,14 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
-            <img src="../src/assets/images/twitter.png" />
+          <a href="https://twitter.com/maldita_desidia">
+            <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
-            <img src="../src/assets/images/github.png" />
+          <a href="https://github.com/jmanuelagudelo">
+            <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
-            <img src="../src/assets/images/instagram.png" />
+          <a href="https://www.instagram.com/maldita_desidia/">
+            <img src="${instagram}" />
           </a>
         </div>
       </div>
